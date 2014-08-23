545429803f739df543fdebf3ef44e36d8c27aa03
Toru <toru.inoue@kissaki.tv>
2014-08-23 21:23:14 +0900

    add sample app.

---
+++ SampleApp/Assets/Editor/XCodePostProcess.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.XCodeEditor;
+
+using UnityEngine;
+
+using System.IO;
+
+public static class XCodePostProcess
+{
+    [PostProcessBuild]
+    public static void OnPostProcessBuild( BuildTarget target, string path )
+    {
+        // Create a new project object from build target
+        XCProject project = new XCProject(path);
+
+        // Find and run through all projmods files to patch the project
+        var files = Directory.GetFiles( Application.dataPath, "*.projmods", SearchOption.AllDirectories );
+        foreach( var file in files ) {
+            project.ApplyMod( file );
+        }
+        
+        // Finally save the xcode project
+        project.Save();
+    }
+}
